22ff72f8cff47835ee9893a7a83a72dd217e0ebe
yyya_nico <yyya_nico@outlook.jp>
2023-08-07 23:35:34 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# Misskey.io_OneLineTimeline
-1ノート1行でMisskey.ioの新しい投稿を表示
-
-一応公開。
+# Misskey.io_MultiTimeline
--- src/index.js
+++ src/index.js
@@ -1,26 +1,51 @@
 import { Stream as misskeyStream } from 'misskey-js';
 import { parseSimple as mfmParseSimple } from 'mfm-js';
+import MagicGrid from 'magic-grid';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const currentOrigin = 'https://misskey.io';
   const stream = new misskeyStream(currentOrigin);
   const hTimeline = stream.useChannel('localTimeline');
+  // const stream = new misskeyStream(currentOrigin, {token: ''});
+  // const hTimeline = stream.useChannel('homeTimeline');
   const containers = document.querySelectorAll('.container');
   const noteList = document.getElementById('notes-list');
   const renoteList = document.getElementById('renotes-list');
+  const mediaList = document.getElementById('media-list');
+  const rnMediaList = document.getElementById('rn-media-list');
   const notelatestBtn = document.getElementById('note-latest');
   const renotelatestBtn = document.getElementById('renote-latest');
+  const mediumlatestBtn = document.getElementById('medium-latest');
+  const rnMediumlatestBtn = document.getElementById('rn-medium-latest');
   const autoShowNew = {
     note: true,
-    renote: true
+    renote: true,
+    medium: true,
+    rnMedium: true
   };
   const stored = {
     notes: [],
-    renotes: []
+    renotes: [],
+    media: [],
+    rnMedia: []
   };
   const emojiShortcodeToUrlDic = {};
   const noteLimit = 150;
+  const mediaMG = new MagicGrid({
+    container: '#media-list',
+    items: 1,
+    gutter: 20,
+    animate: true
+  });
+  const rnMediaMG = new MagicGrid({
+    container: '#rn-media-list',
+    items: 1,
+    gutter: 20,
+    animate: true
+  });
   let wakeLock = null;
+  mediaMG.listen();
+  rnMediaMG.listen();
 
   const scrollToBottom = node => {
     node.scrollTop = node.scrollHeight;
@@ -123,7 +148,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       return `[CW]${await simpleMfmToHTML(htmlspecialchars(note.cw), host)} <span class="cwtext">${(await simpleMfmToHTML(htmlspecialchars(note.text), host)).replace(/\n/g, '<br>')}</span>`;
     } else if (note.text) {
       return (await simpleMfmToHTML(htmlspecialchars(note.text), host)).replace(/\n/g, '<br>');
-    } else if (note.renoteId) {
+    } else if (note.renoteId || note.fileIds.length) {
       return '';
     } else {
       return '<span class="nothing">なにもありません</span>';
@@ -139,60 +164,151 @@ document.addEventListener('DOMContentLoaded', async () => {
     // }
     const formatted = {
       name:      note.user.name ? await simpleMfmToHTML(note.user.name) : note.user.username,
-      planeName: note.user.name ? note.user.name : note.user.username,
+      plainName: note.user.name ? note.user.name : note.user.username,
       text:      await makeTextHTMLFromNote(note),
       fileCount: note.fileIds.length ? `<span class="file-count">[${note.fileIds.length}つのファイル]</span>` : '', 
       time:      new Date(note.createdAt).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit', second: '2-digit'}),
       ...(renote && {
         rnName:      renote.user.name ? await simpleMfmToHTML(renote.user.name, renote.host) : renote.user.username,
-        planeRnName: renote.user.name ? renote.user.name : renote.user.username,
-        rnText:      await makeTextHTMLFromNote(renote),
+        plainRnName: renote.user.name ? renote.user.name : renote.user.username,
+        rnText:      await makeTextHTMLFromNote(renote, renote.host),
         rnFileCount: renote.fileIds.length ? `<span class="file-count">[${renote.fileIds.length}つのファイル]</span>` : '', 
         rnTime: fromNow(new Date(renote.createdAt))
       })
     };
     const html = !renote ? 
     `<li data-id="${note.id}">
-      <span class="wrap"><span class="name" title="${formatted.planeName}">${formatted.name}</span><span class="text">${formatted.text}${formatted.fileCount}</span></span><a href="${currentOrigin}/notes/${note.id}" class="time" target="_blank" rel=”noopener”>${formatted.time}</a>
+      <span class="wrap"><span class="name" title="${formatted.plainName}">${formatted.name}</span><span class="text">${formatted.text}${formatted.fileCount}</span></span><a href="${currentOrigin}/notes/${note.id}" class="time" target="_blank" rel=”noopener”>${formatted.time}</a>
     </li>
     `
     :
     `<li data-id="${note.id}">
       <div class="renote-info">
-        <span class="wrap"><span class="name" title="${formatted.planeName}">${formatted.name}</span><span class="text">${formatted.text}${formatted.fileCount}</span></span><a href="${currentOrigin}/notes/${note.id}" class="time" target="_blank" rel=”noopener”>${formatted.time}</a>
+        <span class="wrap"><span class="name" title="${formatted.plainName}">${formatted.name}</span><span class="text">${formatted.text}${formatted.fileCount}</span></span><a href="${currentOrigin}/notes/${note.id}" class="time" target="_blank" rel=”noopener”>${formatted.time}</a>
       </div>
       <div class="renoted-note">
-        <span class="wrap"><span class="name" title="${formatted.planeRnName}">${formatted.rnName}</span><span class="text">${formatted.rnText}${formatted.rnFileCount}</span></span><a href="${currentOrigin}/notes/${renote.id}" class="time" target="_blank" rel=”noopener”>${formatted.rnTime}</a>
+        <span class="wrap"><span class="name" title="${formatted.plainRnName}">${formatted.rnName}</span><span class="text">${formatted.rnText}${formatted.rnFileCount}</span></span><a href="${currentOrigin}/notes/${renote.id}" class="time" target="_blank" rel=”noopener”>${formatted.rnTime}</a>
       </div>
     </li>
     `;
     return html;
   };
 
+  const makeHTMLFromNoteForMedia = async note => {
+    note.isRenote = Boolean(note.renoteId);
+    const renote = note.isRenote ? note.renote : null;
+    const firstFile = !renote ? note.files[0] : renote.files[0];
+    const formatted = {
+      text:      await makeTextHTMLFromNote(!renote ? note : renote, renote && renote.host),
+      plainText: (note => {return note.cw ? note.cw : note.text ? note.text.replace(/\n/g, '') : ''})(!renote ? note : renote),
+      fileCount: (note => {return note.fileIds.length > 1 ? `<span class="more-file-count">+ ${note.fileIds.length - 1}</span>` : '';})(!renote ? note : renote),
+    };
+    const html = firstFile.type.includes('image') || firstFile.type.includes('video') ?
+    `<li data-id="${note.id}" ${renote ? `data-rn-id="${renote.id}"` : ''}>
+      <a href="${currentOrigin}/notes/${note.id}" class="link" target="_blank" rel=”noopener”>
+        <img src="${firstFile.thumbnailUrl || ''}" alt="${firstFile.comment || firstFile.name}" class="${firstFile.isSensitive ? 'is-sensitive' : ''}">
+        ${firstFile.type.includes('video') ? '<span class="is-video">動画</span>' : ''}
+        ${formatted.fileCount}
+      </a>${formatted.text &&`
+      <div class="text" title="${formatted.plainText}">
+        ${formatted.text}
+      </div>`}
+    </li>
+    `
+    : firstFile.type.includes('audio') ?
+    `<li data-id="${note.id}" ${renote ? `data-rn-id="${renote.id}"` : ''}>
+      <a href="${currentOrigin}/notes/${note.id}" class="link" target="_blank" rel=”noopener”>
+        音声
+        ${formatted.fileCount}
+      </a>${formatted.text &&`
+      <div class="text" title="${formatted.plainText}">
+        ${formatted.text}
+      </div>`}
+    </li>
+    `
+    : 
+    `<li data-id="${note.id}" ${renote ? `data-rn-id="${renote.id}"` : ''}>
+      <a href="${currentOrigin}/notes/${note.id}" class="link" target="_blank" rel=”noopener”>
+        その他:${firstFile.type}
+        ${formatted.fileCount}
+      </a>${formatted.text &&`
+      <div class="text" title="${formatted.plainText}">
+        ${formatted.text}
+      </div>`}
+    </li>
+    `;
+    return html;
+  };
+
   Node.prototype.appendToTl = async function(noteOrNotes) {
-    if (this !== noteList && this !== renoteList) {
+    if (this !== noteList && this !== renoteList && this !== mediaList && this !== rnMediaList) {
       return false;
     }
     // console.log(note);
     const isNote = !Array.isArray(noteOrNotes);
     let html = '';
-    if (isNote) {
-      const note = noteOrNotes;
-      html = await makeHTMLFromNote(note);
-    } else {
-      const notes = noteOrNotes;
-      while (notes.length) {
-        html += await makeHTMLFromNote(notes.shift());
-        // console.log('note shifted, notes count:', notes.length);
+    if (this === noteList || this === renoteList ) {
+      if (isNote) {
+        const note = noteOrNotes;
+        html = await makeHTMLFromNote(note);
+      } else {
+        const notes = noteOrNotes;
+        while (notes.length) {
+          html += await makeHTMLFromNote(notes.shift());
+          // console.log('note shifted, notes count:', notes.length);
+        }
       }
-    }
-    this.insertAdjacentHTML('beforeend', html);
-    // console.log('note count:',this.querySelectorAll('li').length);
-    if (autoShowNew[this === noteList && 'note' || this === renoteList && 'renote']) {
-      scrollToBottom(this.parentElement);
-    }
-    while (this.querySelectorAll('li').length > noteLimit) {
-      this.firstElementChild.remove();
+      this.insertAdjacentHTML('beforeend', html);
+      // console.log('note count:',this.querySelectorAll('li').length);
+      if (autoShowNew[this === noteList && 'note' || this === renoteList && 'renote']) {
+        scrollToBottom(this.parentElement);
+      }
+      while (this.querySelectorAll('li').length > noteLimit) {
+        this.firstElementChild.remove();
+      }
+    } else if (this === mediaList || this === rnMediaList) {
+      if (isNote) {
+        const note = noteOrNotes;
+        html = await makeHTMLFromNoteForMedia(note);
+        if (this === rnMediaList) {
+          const alreadyRN = rnMediaList.querySelector(`[data-rn-id="${note.renoteId}"]`);
+          alreadyRN && alreadyRN.remove();
+        }
+      } else {
+        const notes = noteOrNotes;
+        while (notes.length) {
+          const note = notes.shift();
+          html = `${await makeHTMLFromNoteForMedia(note)}${html}`;
+          // console.log('note shifted, notes count:', notes.length);
+          if (this === rnMediaList) { // TODO: RN内容に複数重複があったときに最新のリノートを除いて除去したい
+            const alreadyRN = rnMediaList.querySelector(`[data-rn-id="${note.renoteId}"]`);
+            alreadyRN && alreadyRN.remove();
+          }
+        }
+      }
+      this.insertAdjacentHTML('afterbegin', html);
+      if (autoShowNew[this === mediaList && 'medium' || this === rnMediaList && 'rnMedium']) {
+        this.parentElement.scrollTop = 0;
+      }
+      const img = this.querySelector('img');
+      if (img && img.src !== '') {
+        img.addEventListener('load', () => {
+          if (this === mediaList) {
+            mediaMG.positionItems();
+          } else if (this === rnMediaList) {
+            rnMediaMG.positionItems();
+          }
+        });
+      } else {
+        if (this === mediaList) {
+          mediaMG.positionItems();
+        } else if (this === rnMediaList) {
+          rnMediaMG.positionItems();
+        }
+      }
+      while (this.querySelectorAll('li').length > noteLimit) {
+        this.lastElementChild.remove();
+      }
     }
   }
 
@@ -219,19 +335,42 @@ document.addEventListener('DOMContentLoaded', async () => {
     // console.log(note);
     const isRenote = Boolean(note.renoteId);
     const kind = !isRenote ? 'note' : 'renote';
-      if (autoShowNew[kind]) {
-        if (!isRenote) {
-          await noteList.appendToTl(note);//RN以外
-        }else {
-          await renoteList.appendToTl(note);//RN
-        }
+    if (autoShowNew[kind]) {
+      if (!isRenote) {
+        await noteList.appendToTl(note);//RN以外
+      }else {
+        await renoteList.appendToTl(note);//RN
+      }
+    } else {
+      stored[`${kind}s`].push(note);
+      // console.log(kind + ' pushed, stored ' + kind +'s count:',stored[`${kind}s`].length);
+      if (stored[`${kind}s`].length > noteLimit) {
+        stored[`${kind}s`] = stored[`${kind}s`].slice(-noteLimit);
+      }
+    }
+    // const mediumKind = !isRenote ? ['medium', 'media'] : ['rnMedium', 'rnMedia'];
+    if (note.fileIds.length) {
+      if (autoShowNew.medium) {
+        await mediaList.appendToTl(note);
       } else {
-        stored[`${kind}s`].push(note);
-        // console.log(kind + ' pushed, stored ' + kind +'s count:',stored[`${kind}s`].length);
-        if (stored[`${kind}s`].length > noteLimit) {
-          stored[`${kind}s`] = stored[`${kind}s`].slice(-noteLimit);
+        stored.media.push(note);
+        // console.log('media pushed, stored media count:',stored.media.length);
+        if (stored.media.length > noteLimit) {
+          stored.media = stored.media.slice(-noteLimit);
         }
       }
+    }
+    if (isRenote && note.renote.fileIds.length) {
+      if (autoShowNew.rnMedium) {
+        await rnMediaList.appendToTl(note);
+      } else {
+        stored.rnMedia.push(note);
+        // console.log('rnMedia pushed, stored rnMedia count:',stored.rnMedia.length);
+        if (stored.rnMedia.length > noteLimit) {
+          stored.rnMedia = stored.rnMedia.slice(-noteLimit);
+        }
+      }
+    }
   });
 
   const textToggleHandler = e => {
@@ -252,33 +391,61 @@ document.addEventListener('DOMContentLoaded', async () => {
     } else if (latestBtn === renotelatestBtn) {
       autoShowNew.renote = true;
       await renoteList.appendToTl(stored.renotes);//RN
+    } else if (latestBtn === mediumlatestBtn) {
+      autoShowNew.medium = true;
+      await mediaList.appendToTl(stored.media);//Media
+    } else if (latestBtn === rnMediumlatestBtn) {
+      autoShowNew.rnMedium = true;
+      await rnMediaList.appendToTl(stored.rnMedia);//RNMedia
     }
     latestBtn.textContent = '新しいノートを見る';
   }
 
   notelatestBtn.addEventListener('click', latestBtnHandler);
   renotelatestBtn.addEventListener('click', latestBtnHandler);
+  mediumlatestBtn.addEventListener('click', latestBtnHandler);
+  rnMediumlatestBtn.addEventListener('click', latestBtnHandler);
 
   containers.forEach(container => {
     container.addEventListener('scroll', async e => {
       const latestBtn = container.querySelector('button[id$="-latest"]');
-      const containerRole = container.classList.contains('notes')   ? 'notes'
-                          : container.classList.contains('renotes') ? 'renotes'
-                                                                    : null;
+      const containerRole = container.classList.contains('notes')    ? 'notes'
+                          : container.classList.contains('renotes')  ? 'renotes'
+                          : container.classList.contains('media')    ? 'media'
+                          : container.classList.contains('rn-media') ? 'rn-media'
+                                                                     : null;
       // console.log('scrolled: '+ containerRole);
-      if (container.scrollHeight - container.clientHeight - container.scrollTop >= 3) {
-        autoShowNew[containerRole.slice(0,-1)] = false;
-        latestBtn.classList.add('show');
-      } else {
-        if (stored[containerRole].length) {
-          if (containerRole === 'notes') {
-            await noteList.appendToTl(stored.notes);//RN以外
-          } else if (containerRole === 'renotes') {
-            await renoteList.appendToTl(stored.renotes);//RN
-          }
+      if (containerRole === 'notes' || containerRole === 'renotes') {
+        if (container.scrollHeight - container.clientHeight - container.scrollTop >= 3) {
+          autoShowNew[containerRole.slice(0,-1)] = false;
+          latestBtn.classList.add('show');
         } else {
-          autoShowNew[containerRole.slice(0,-1)] = true;
-          latestBtn.classList.remove('show');
+          if (stored[containerRole].length) {
+            if (containerRole === 'notes') {
+              await noteList.appendToTl(stored.notes);//RN以外
+            } else if (containerRole === 'renotes') {
+              await renoteList.appendToTl(stored.renotes);//RN
+            }
+          } else {
+            autoShowNew[containerRole.slice(0,-1)] = true;
+            latestBtn.classList.remove('show');
+          }
+        }
+      } else if (containerRole === 'media' || containerRole === 'rn-media') {
+        if (container.scrollTop > 1) {
+          autoShowNew[containerRole === 'media' && 'medium' || containerRole === 'rn-media' && 'rnMedium'] = false;
+          latestBtn.classList.add('show');
+        } else {
+          if (stored[containerRole === 'media' && 'media' || containerRole === 'rn-media' && 'rnMedia'].length) {
+            if (containerRole === 'media') {
+              await mediaList.appendToTl(stored.media);//Media
+            } else if (containerRole === 'renotes') {
+              await rnMediaList.appendToTl(stored.rnMedia);//RNMedia
+            }
+          } else {
+            autoShowNew[containerRole === 'media' && 'medium' || containerRole === 'rn-media' && 'rnMedium'] = true;
+            latestBtn.classList.remove('show');
+          }
         }
       }
     });
