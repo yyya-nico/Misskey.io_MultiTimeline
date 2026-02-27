52787e0c05153831306540dd30fa6ffb03a655b3
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2026-02-27 20:46:20 +0900

    emoji-store調整

--- emoji-store.js
+++ emoji-store.js
@@ -1,42 +1,34 @@
 import { api as misskeyApi } from 'misskey-js';
 
 const emojiShortcodeToUrlDic = {};
-const originToHost = origin => origin.replace('https://', '');
+const originToHost = (origin) => origin.replace('https://', '');
 
-export const createEmojiStore = origin => {
+export const createEmojiStore = (origin) => {
   const localHost = originToHost(origin);
 
   const initEmojis = async () => {
     const host = originToHost(origin);
-    const moreThan24HoursHavePassed =
-      Date.now() - Number(localStorage.getItem(`tlEmojis${host}Loaded`)) >= 24 * 60 * 60 * 1000;
+    const moreThan24HoursHavePassed = Date.now() - Number(localStorage.getItem(`tlEmojis${host}Loaded`)) >= 24 * 60 * 60 * 1000;
 
-    if (
-      localStorage.getItem(`tlEmojis${host}`) &&
-      localStorage.getItem(`tlEmojis${host}Loaded`) &&
-      !moreThan24HoursHavePassed
-    ) {
+    if (localStorage.getItem(`tlEmojis${host}`) && localStorage.getItem(`tlEmojis${host}Loaded`) && !moreThan24HoursHavePassed) {
       emojiShortcodeToUrlDic[host] = JSON.parse(localStorage.getItem(`tlEmojis${host}`));
       console.log('Loaded emojis from cache');
       return;
     }
 
     emojiShortcodeToUrlDic[host] = {};
-    await fetch(`${origin}/api/emojis`)
-      .then(async response => {
-        const data = await response.json();
-        if (response.status === 200) {
-          const emojis = data.emojis;
-          emojis.forEach(entry => {
-            emojiShortcodeToUrlDic[host][entry.name] = entry.url;
-          });
-          localStorage.setItem(`tlEmojis${host}`, JSON.stringify(emojiShortcodeToUrlDic[host]));
-          localStorage.setItem(`tlEmojis${host}Loaded`, Date.now());
-        } else {
-          console.log('error or no content', response.status);
-        }
+    const cli = new misskeyApi.APIClient({ origin });
+    await cli
+      .request('emojis', {})
+      .then((data) => {
+        const emojis = data.emojis;
+        emojis.forEach((entry) => {
+          emojiShortcodeToUrlDic[host][entry.name] = entry.url;
+        });
+        localStorage.setItem(`tlEmojis${host}`, JSON.stringify(emojiShortcodeToUrlDic[host]));
+        localStorage.setItem(`tlEmojis${host}Loaded`, Date.now());
       })
-      .catch(e => {
+      .catch((e) => {
         console.error('Failed to load Emojis', e);
       });
   };
@@ -46,22 +38,22 @@ export const createEmojiStore = origin => {
     const cli = new misskeyApi.APIClient({ origin: `https://${host}` });
     await cli
       .request('emoji', { name: name })
-      .then(data => {
+      .then((data) => {
         const isValid = data !== null && (host === localHost || data.localOnly !== true);
         emojiShortcodeToUrlDic[host][name] = isValid ? data.url : null;
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(`${host}\'s Emoji`, `:${name}:`, 'not found');
         console.dir(e);
         emojiShortcodeToUrlDic[host][name] = null;
       });
   };
 
-  const storeExternalEmojisFromNote = note => {
+  const storeExternalEmojisFromNote = (note) => {
     const host = note.user.host;
     emojiShortcodeToUrlDic[host] = emojiShortcodeToUrlDic[host] || {};
-    [note.user.emojis, note.emojis].forEach(emojis => {
-      Object.keys(emojis).forEach(name => {
+    [note.user.emojis, note.emojis].forEach((emojis) => {
+      Object.keys(emojis).forEach((name) => {
         const url = emojis[name];
         emojiShortcodeToUrlDic[host][name] = url;
       });
@@ -70,10 +62,7 @@ export const createEmojiStore = origin => {
 
   const emojiShortcodeToUrl = async (name, host) => {
     const resolvedHost = host || localHost;
-    if (
-      !(resolvedHost in emojiShortcodeToUrlDic) ||
-      !(name in emojiShortcodeToUrlDic[resolvedHost])
-    ) {
+    if (!(resolvedHost in emojiShortcodeToUrlDic) || !(name in emojiShortcodeToUrlDic[resolvedHost])) {
       await loadAndStoreEmoji(name, resolvedHost);
     }
     return emojiShortcodeToUrlDic[resolvedHost][name] || null;
@@ -82,6 +71,6 @@ export const createEmojiStore = origin => {
   return {
     initEmojis,
     storeExternalEmojisFromNote,
-    emojiShortcodeToUrl
+    emojiShortcodeToUrl,
   };
 };
